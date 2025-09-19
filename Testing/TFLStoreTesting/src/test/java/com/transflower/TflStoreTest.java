fc08ba155e0ab3ccb57c52a0dd6539d8adcc0fcb
sanika-bhor <bhorsanika0239@gmail.com>
2025-09-19 23:17:42 +0530

    test case writing

---
+++ Testing/TFLStoreTesting/src/test/java/com/transflower/TflStoreTest.java
@@ -0,0 +1,43 @@
+package com.transflower;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TflStoreTest {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://localhost:8000/index.html");
+    }
+
+    @Test
+    void indexState() throws InterruptedException {
+        WebElement gallery = driver.findElement(By.id("galleryLink"));
+        gallery.click();
+
+        Thread.sleep(2000); 
+
+        String currentLink = driver.getCurrentUrl();
+        assertTrue(currentLink.contains("catalog.html"));
+    }
+
+    @AfterAll
+    static void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
