4f51937b5105323ce1518630bed0f7dd929f2523
sanika-bhor <bhorsanika0239@gmail.com>
2025-10-04 12:30:31 +0530

    RestAPI testing for delete

--- Testing/RestAPITesting/src/test/java/com/tap/example/RestAPITesting/ProductApiTest.java
+++ Testing/RestAPITesting/src/test/java/com/tap/example/RestAPITesting/ProductApiTest.java
@@ -2,6 +2,7 @@ package com.tap.example.RestAPITesting;
 
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.*;
 
 import org.testng.annotations.Test;
@@ -30,7 +31,7 @@ public class ProductApiTest {
     {
         RestAssured.baseURI="http://localhost:9090";
         given()
-        .when().get("/api/products/1")
+        .when().get("/api/products/3")
         .then()
         .statusCode(200)
         .body("name", notNullValue())
@@ -58,5 +59,38 @@ public class ProductApiTest {
                         .statusCode(200);
 }
 
+
+@Test
+public void updateProduct_shouldReturn200()
+{
+    RestAssured.baseURI="http://localhost:9090";
+    String updateProductJson="""
+            {
+                    "id": 1,
+                    "name": "Wireless",
+                    "price": 19000
+                }
+            """;
+
+        given()
+        .contentType(ContentType.JSON)
+        .body(updateProductJson)
+        .when()
+        .put("/api/products/1")
+        .then()
+        .statusCode(200);
+}
+
+@Test
+public void deleteProduct_shouldReturn200()
+{
+    RestAssured.baseURI="http://localhost:9090";
+    given()
+    .when()
+    .delete("/api/products/2")
+    .then()
+    .statusCode(200)
+    .body(equalTo("true"));
+}
 
 }
