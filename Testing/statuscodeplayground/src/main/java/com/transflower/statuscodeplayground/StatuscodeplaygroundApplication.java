cc2a1caf6d4e8951b6a9f33633e96fbb12222d25
sanika-bhor <bhorsanika0239@gmail.com>
2025-10-09 15:16:25 +0530

    status code playground project

---
+++ Testing/statuscodeplayground/src/main/java/com/transflower/statuscodeplayground/StatuscodeplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.transflower.statuscodeplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatuscodeplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatuscodeplaygroundApplication.class, args);
+	}
+
+}
---
+++ Testing/statuscodeplayground/src/test/java/com/transflower/statuscodeplayground/StatuscodeplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transflower.statuscodeplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatuscodeplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
