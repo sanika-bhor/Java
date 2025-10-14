95dede5172cd2bc1c1f61396dd09e5995f21d538
sanika-bhor <bhorsanika0239@gmail.com>
2025-10-14 19:06:18 +0530

    changes for cucmber testing

--- Testing/statuscodeplayground/src/test/java/com/transflower/statuscodeplayground/stepdefs/CucumberTestRunner.java
+++ Testing/statuscodeplayground/src/test/java/com/transflower/statuscodeplayground/stepdefs/CucumberTestRunner.java
@@ -1,7 +1,12 @@
 package com.transflower.statuscodeplayground.stepdefs;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@Cucumber
-public class CucumberTestRunner {
+
+@CucumberOptions(features = "src/test/resources/features", glue = {
+        "com.transflower.statuscodeplayground.stepdefs" }, plugin = { "pretty",
+                "html:target/cucumber-report.html" }, monochrome = true)
+public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+    
 }
--- Testing/statuscodeplayground/src/test/java/com/transflower/statuscodeplayground/stepdefs/StatusStepDefinitions.java
+++ Testing/statuscodeplayground/src/test/java/com/transflower/statuscodeplayground/stepdefs/StatusStepDefinitions.java
@@ -1,45 +1,42 @@
 package com.transflower.statuscodeplayground.stepdefs;
 
-import io.cucumber.java.en.And;
+import io.restassured.response.Response;
+
+import org.springframework.web.client.RestTemplate;
+import static io.restassured.RestAssured.given;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-// import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
+import io.restassured.RestAssured;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StatusStepDefinitions {
 
-    @LocalServerPort
-    private int port;
-
-    private ResponseEntity<String> response;
+    private Response response;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Given("the Product API is available")
+    public void the_product_api_is_available() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
     @When("I send GET request to {string}")
     public void iSendGetRequestTo(String path) {
-        String url = "http://localhost:" + port + path;
-        response = restTemplate.getForEntity(url, String.class);
+        String url = "http://localhost:8080" + path;
+        response = given().when().get(url);
+
     }
 
     @When("I send POST request to {string}")
     public void iSendPostRequestTo(String path) {
-        String url = "http://localhost:" + port + path;
-        response = restTemplate.postForEntity(url, null, String.class);
+        String url = "http://localhost:8080" + path;
+        response = given().when().post(url);
     }
 
     @Then("the response status should be {int}")
-    public void theResponseStatusShouldBe(int expectedStatus) {
-        assertEquals(expectedStatus, response.getStatusCodeValue());
-    }
+    public void theResponseStatusShouldBe(int statusCode) {
+        response.then().statusCode(statusCode);
 
-    @And("the response should contain {string}")
-    public void theResponseShouldContain(String expectedText) {
-        assertTrue(response.getBody().toLowerCase().contains(expectedText.toLowerCase()));
     }
 }
+
